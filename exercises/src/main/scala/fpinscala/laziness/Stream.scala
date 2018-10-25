062ec0e86ec252b636a046dd8e9416f0fb0f3858
Alessandro Borsoi <alessandro.borsoi1984@gmail.com>
2018-10-25 10:08:12 +0200

    Chapter 5 WIP

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -8,14 +8,18 @@ import scala.{Stream => _}
 
 trait Stream[+A] {
 
-  def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
-    this match {
-      case Cons(h, t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
-      case _ => z
-    }
+  // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name
+  // and may choose not to evaluate it.
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+    // If `f` doesn't evaluate its second argument, the recursion never occurs.
+    case Cons(h, t) => f(h(), t().foldRight(z)(f))
+    case _ => z
+  }
 
+  // Here `b` is the unevaluated recursive step that folds the tail of the stream.
+  // If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
   def exists(p: A => Boolean): Boolean =
-    foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
+    foldRight(false)((a, b) => p(a) || b)
 
   @annotation.tailrec
   final def find(f: A => Boolean): Option[A] = this match {
@@ -40,17 +44,33 @@ trait Stream[+A] {
     case _ => this
   }
 
-  def takeWhile(p: A => Boolean): Stream[A] = this match {
-    case Cons(h, t) if p(h()) => cons(h(), t().takeWhile(p))
-    case _ => empty
-  }
+  def takeWhile(p: A => Boolean): Stream[A] =
+    foldRight(empty[A])((h, t) => if (p(h)) cons(h, t) else empty)
 
-  def forAll(p: A => Boolean): Boolean = ???
+  //  this match {
+  //    case Cons(h, t) if p(h()) => cons(h(), t().takeWhile(p))
+  //    case _ => empty
+  //  }
 
-  def headOption: Option[A] = ???
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true)((a, b) => p(a) && b)
+
+  def headOption: Option[A] =
+    foldRight(None: Option[A])((h, _) => Some(h))
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
+  def map[B](f: A => B): Stream[B] =
+    foldRight(empty[B])((h, t) => cons(f(h), t))
+
+  def filter(p: A => Boolean): Stream[A] =
+    foldRight(empty[A])((h, t) => if (p(h)) cons(h, t) else t)
+
+  def append[B >: A](s: => Stream[B]): Stream[B] =
+    foldRight(s)((h, t) => cons(h, t))
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] =
+    foldRight(empty[B])((h, t) => f(h) append t)
 
   def startsWith[B](s: Stream[B]): Boolean = ???
 }
--- exercises/src/test/scala/fpinscala/laziness/StreamTest.scala
+++ exercises/src/test/scala/fpinscala/laziness/StreamTest.scala
@@ -21,4 +21,26 @@ class StreamTest extends FunSuite {
     assert(Stream(2, 4, 6, 8, 11).takeWhile(_ % 2 == 0).toList == List(2, 4, 6, 8))
   }
 
+  test("testForAll") {
+    assert(Stream(1, 2, 3, 4, 5).forAll(_ > 0))
+    assert(!Stream(1, 2, 3, 4, 5).forAll(_ % 2 == 0))
+  }
+
+  test("testMap") {
+    assert(Stream(1, 2, 3, 4, 5).map(_ + 10).toList == List(11, 12, 13, 14, 15))
+  }
+
+  test("testFilter") {
+    assert(Stream(1, 2, 3, 4, 5).filter(_ < 0).toList == List())
+    assert(Stream(1, 2, 3, 4, 5).filter(_ % 2 == 0).toList == List(2, 4))
+  }
+
+  test("testAppend") {
+    assert(Stream(1, 2, 3).append(Stream(4, 5, 6)).toList == List(1, 2, 3, 4, 5, 6))
+  }
+
+  test("testFlatMap") {
+    assert(Stream(1, 2, 3, 4, 5).flatMap(Stream(_)).toList == List(1, 2, 3, 4, 5))
+  }
+
 }
