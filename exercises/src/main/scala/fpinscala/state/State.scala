7dfe3c8ad37071ff9d4a334926f67c76c193a6c1
Alessandro Borsoi <alessandro.borsoi1984@gmail.com>
2018-10-26 12:14:02 +0200

    Chapter 6

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -112,11 +112,37 @@ object RNG {
 
 case class State[S, +A](run: S => (A, S)) {
 
-  def map[B](f: A => B): State[S, B] = ???
+  def map[B](f: A => B): State[S, B] =
+    flatMap(a => State.unit(f(a)))
 
-  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] = ???
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap(a => sb.map(b => f(a, b)))
 
-  def flatMap[B](f: A => State[S, B]): State[S, B] = ???
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+    val (a, s1) = run(s)
+    f(a).run(s1)
+  })
+
+}
+
+object State {
+
+  type Rand[A] = State[RNG, A]
+
+  def unit[S, A](a: A): State[S, A] =
+    State(s => (a, s))
+
+  def sequence[S, A](sas: List[State[S, A]]): State[S, List[A]] =
+    sas.foldRight(unit[S, List[A]](List()))((f, acc) => f.map2(acc)(_ :: _))
+
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
+
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
 
 }
 
@@ -128,8 +154,26 @@ case object Turn extends Input
 
 case class Machine(locked: Boolean, candies: Int, coins: Int)
 
-object State {
-  type Rand[A] = State[RNG, A]
+object Candy {
 
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+  import fpinscala.state.State._
+
+  def update: Input => Machine => Machine =
+    (i: Input) => (s: Machine) =>
+      (i, s) match {
+        case (_, Machine(_, 0, _)) => s
+        case (Coin, Machine(false, _, _)) => s
+        case (Turn, Machine(true, _, _)) => s
+        case (Coin, Machine(true, candy, coin)) =>
+          Machine(locked = false, candy, coin + 1)
+        case (Turn, Machine(false, candy, coin)) =>
+          Machine(locked = true, candy - 1, coin)
+      }
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] =
+    for {
+      _ <- sequence(inputs map (modify[Machine] _ compose update))
+      s <- get
+    } yield (s.coins, s.candies)
+
 }
