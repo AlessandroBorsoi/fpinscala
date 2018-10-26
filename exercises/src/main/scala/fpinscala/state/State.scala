a06ec217fae52cec10b95a32a787acb38bc85c50
Alessandro Borsoi <alessandro.borsoi1984@gmail.com>
2018-10-26 11:41:02 +0200

    Chapter 6 WIP

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -69,11 +69,45 @@ object RNG {
     loop(count, Nil, rng)
   }
 
-  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def doubleWithMap: Rand[Double] =
+    map(nonNegativeInt)(_ / (Int.MaxValue.toDouble + 1))
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, r1) = ra(rng)
+      val (b, r2) = rb(r1)
+      (f(a, b), r2)
+    }
 
-  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    map2(ra, rb)((_, _))
+
+  val randIntDouble: Rand[(Int, Double)] = both(int, double)
+
+  val randDoubleInt: Rand[(Double, Int)] = both(double, int)
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+    val (a, r1) = f(rng)
+    g(a)(r1)
+  }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0) unit(mod)
+      else nonNegativeLessThan(n)
+    }
+  }
+
+  def mapWithFlatMap[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
+
+  def map2WithFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => map(rb)(b => f(a, b)))
+
 }
 
 case class State[S, +A](run: S => (A, S)) {
--- exercises/src/test/scala/fpinscala/state/RNGTest.scala
+++ exercises/src/test/scala/fpinscala/state/RNGTest.scala
@@ -11,4 +11,19 @@ class RNGTest extends FunSuite {
     assert(int > 0)
   }
 
+  test("testDouble") {
+    val (d, _) = double(SimpleRNG(42))
+    assert(0 < d && d < 1)
+  }
+
+  test("testDoubleWithMap") {
+    val dwm: Rand[Double] = doubleWithMap
+    val (d, _) = dwm(SimpleRNG(42))
+    assert(d > 0 && d < 1)
+  }
+
+  test("testSequence") {
+    assert(sequence(List(unit(1), unit(2), unit(3)))(SimpleRNG(42))._1 == List(1, 2, 3))
+  }
+
 }
